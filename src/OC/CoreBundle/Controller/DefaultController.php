0cc7d5a901e6b6f08527d7953030f50002fc16cc
rg <rgrabarski@gmail.com>
2014-11-06 10:35:07 +0100

    first commit

---
+++ src/OC/CoreBundle/Controller/DefaultController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace OC\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        $listAdverts = array(
+            array(
+                'title'   => 'Recherche développpeur Symfony2',
+                'id'      => 1,
+                'author'  => 'Alexandre',
+                'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+                'date'    => new \Datetime()),
+            array(
+                'title'   => 'Mission de webmaster',
+                'id'      => 2,
+                'author'  => 'Hugo',
+                'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+                'date'    => new \Datetime()),
+            array(
+                'title'   => 'Offre de stage webdesigner',
+                'id'      => 3,
+                'author'  => 'Mathieu',
+                'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+                'date'    => new \Datetime())
+        );
+
+        return $this->render('OCCoreBundle:Default:index.html.twig', array(
+            'listAdverts' => $listAdverts
+        ));
+    }
+
+    public function contactAction(Request $request)
+    {
+        $session = $request->getSession();
+        $session->getFlashBag()->add('info', 'La page de contact n\'est pas encore disponible, merci de revenir plus tard');
+        return $this->redirect($this->generateUrl('oc_core_homepage'));
+    }
+}
